a58f8353edfa48a33baf1649c899ac376d07c457
StepanSologub <stepan.sologub@gmail.com>
2024-02-04 02:18:50 +0700

    refactoring. solved no-param-reassign and no-shadow

--- src/index.js
+++ src/index.js
@@ -17,30 +17,25 @@ const game = {
   over: false,
 };
 
-const greeting = (user) => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   user.name = client.askForName();
   console.log(`Hello, ${user.name}!`);
 };
 
-const setUp = (game) => {
+const setUp = () => {
   game.numbers = [math.getRandomInt(game.maxValue), math.getRandomInt(game.maxValue)];
   game.operation = math.getRandomOperation();
   game.progression = math.getProgression(game.progressionLength, game.maxValue);
 };
 
-const checkModes = (game, mode) => {
+const checkModes = (mode) => {
   console.log(`Question: ${game.numbers[0]}`);
   if (mode === 'even') game.correctAnswer = math.isEven(game.numbers[0]) ? 'yes' : 'no';
   else if (mode === 'prime') game.correctAnswer = math.isPrime(game.numbers[0]) ? 'yes' : 'no';
 };
 
-// const runEvenMode = (game) => {
-//   console.log(`Question: ${game.numbers[0]}`);
-//   game.correctAnswer = math.isEven(game.numbers[0]) ? 'yes' : 'no';
-// };
-
-const runCalcMode = (game) => {
+const runCalcMode = () => {
   console.log(`Question: ${game.numbers[0]} ${game.operation} ${game.numbers[1]}`);
   switch (game.operation) {
     case '+':
@@ -56,12 +51,12 @@ const runCalcMode = (game) => {
   }
 };
 
-const runGCDMode = (game) => {
+const runGCDMode = () => {
   console.log(`Question: ${game.numbers[0]} ${game.numbers[1]}`);
   game.correctAnswer = String(math.getGCD(game.numbers[0], game.numbers[1]));
 };
 
-const runProgressionMode = (game) => {
+const runProgressionMode = () => {
   let elementPosition = 0;
   let question = '';
   elementPosition = math.getRandomInt(game.progressionLength - 1);
@@ -74,23 +69,18 @@ const runProgressionMode = (game) => {
   console.log(question);
 };
 
-// const runPrimeMode = (game) => {
-//   console.log(`Question: ${game.numbers[0]}`);
-//   game.correctAnswer = math.isPrime(game.numbers[0]) ? 'yes' : 'no';
-// };
-
-const generateQuestion = (game, mode) => {
+const generateQuestion = (mode) => {
   switch (mode) {
-    case 'even': checkModes(game, mode); break;
-    case 'calc': runCalcMode(game); break;
-    case 'gcd': runGCDMode(game); break;
-    case 'progression': runProgressionMode(game); break;
-    case 'prime': checkModes(game, mode); break;
+    case 'even': checkModes(mode); break;
+    case 'calc': runCalcMode(); break;
+    case 'gcd': runGCDMode(); break;
+    case 'progression': runProgressionMode(); break;
+    case 'prime': checkModes(mode); break;
     default: break;
   }
 };
 
-const processAnswer = (game, user) => {
+const gameAnswer = () => {
   user.answer = client.getAnswer();
   if (user.answer === game.correctAnswer) {
     console.log('Correct!');
@@ -102,21 +92,21 @@ const processAnswer = (game, user) => {
   }
 };
 
-const congratulation = (game, user) => {
+const congratulation = () => {
   if (game.score === 3) console.log(`Congratulations, ${user.name}!`);
 };
 
 const runGame = (instruction, mode) => {
-  greeting(user);
+  greeting();
   console.log(instruction);
 
   for (let question = 1; question <= 3; question += 1) {
-    setUp(game);
-    generateQuestion(game, mode);
-    processAnswer(game, user);
+    setUp();
+    generateQuestion(mode);
+    gameAnswer();
     if (game.over) break;
   }
-  congratulation(game, user);
+  congratulation();
 };
 
 export default runGame;
